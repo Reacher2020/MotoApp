ab31a49898b84e014e5e7224c7f1f60131a2bc29
Paweł Sawicki <pksawicki@gmail.com>
2024-04-06 09:45:01 +0200

    commit 1

--- BasicStack.cs
+++ BasicStack.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MotoApp
+﻿namespace MotoApp
 {
     internal class BasicStack
     {
@@ -14,9 +8,9 @@ namespace MotoApp
 
         public BasicStack() => _item = new double[10];
         
-        public int Count = _currentIndex + 1;
+        public int Count() => _currentIndex + 1;
 
-        public void Push(double item) => _item[++ _currentIndex] = item;
+        public void Push(double item) => _item[++_currentIndex] = item;
 
         public double Pop() => _item[_currentIndex--];
     }
